76c36df2510af69a12fb8cc0d3db1a375c2fd58b
Benjamin Chew <benjamin.chew@etu.u-bordeaux.fr>
2023-01-20 10:59:51 +0100

    new website

---
+++ _app/immutable/chunks/_page-1a92782e.js
@@ -0,0 +1,1 @@
+import{H as t}from"./control-e7f5239e.js";import{p as o}from"./projects-e0361469.js";function n(e,r){return new t(e,r)}new TextEncoder;function l({params:e}){if(o[e.slug])return o[e.slug];throw n(404,"Not found")}const p=Object.freeze(Object.defineProperty({__proto__:null,load:l},Symbol.toStringTag,{value:"Module"}));export{p as _,l};
