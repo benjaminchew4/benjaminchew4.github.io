d15a379440751be158f1d13b63a5e9dac3d6361d
Benjamin Chew <benjamin.chew@etu.u-bordeaux.fr>
2023-01-20 10:59:51 +0100

    new website

---
+++ _app/immutable/chunks/2-9d1ca27b.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-68995ca0.js";export{t as component};
---
+++ _app/immutable/modules/pages/projects/_slug_/_page.js-556c937c.js
@@ -0,0 +1,1 @@
+import{l}from"../../../../chunks/_page-1a92782e.js";import"../../../../chunks/projects-e0361469.js";export{l as load};
